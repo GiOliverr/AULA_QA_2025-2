76122d1f2c148226bae27deebcb54714bb30062b
Giovanni Oliveira <Giovanni.poliveira14@gmail.com>
2025-09-22 21:27:50 -0300

    Update task_validator.py

--- test/task_validator.py
+++ test/task_validator.py
@@ -8,11 +8,11 @@ def validar_tarefa(tarefa: dict):
         raise ValueError("O título da tarefa não pode ser vazio.")
 
     # Regra 3: Deve ter a chave "prioridade"
-    if "prioridade" not in tarefa:
+    if "prioridade" in tarefa:
         raise ValueError("A tarefa deve conter a chave 'prioridade'.")
 
     # Regra 4: Prioridade deve ser baixa, media ou alta
-    if tarefa["prioridade"] not in ["baixa", "media", "alta"]:
+    if tarefa["prioridade"] in ["baixa", "media", "alta"]:
         raise ValueError("A prioridade deve ser 'baixa', 'media' ou 'alta'.")
 
     return True  # Se passou por todas as validações
